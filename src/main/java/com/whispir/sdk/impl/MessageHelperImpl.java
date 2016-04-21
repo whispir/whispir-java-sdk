2838400ec9bcd02b6ee84c73ed617e23db3ea1d0
jwalsh <jwalsh@whispir.com>
2016-04-21 15:49:14 +1000

    Updated tests, readme and pom.xml to update the version

--- src/main/java/com/whispir/sdk/impl/MessageHelperImpl.java
+++ src/main/java/com/whispir/sdk/impl/MessageHelperImpl.java
@@ -16,289 +16,289 @@ import com.whispir.sdk.interfaces.MessageHelper;
 
 public class MessageHelperImpl extends BaseHelperImpl implements MessageHelper {
 
-	public MessageHelperImpl(WhispirSDK sdk) {
-		super(sdk);
-	}
+  public MessageHelperImpl(WhispirSDK sdk) {
+    super(sdk);
+  }
 
-	/**
-	 * <p>
-	 * Allows a user to send a message in the default My Company workspace.
-	 * </p>
-	 * <p>
-	 * For more complex content, the user should use the Map content overloaded
-	 * function.
-	 * </p>
-	 * 
-	 * @param recipient
-	 *            - the mobile number or email address of the recipient of the
-	 *            message
-	 * @param subject
-	 *            - the textual subject of the message
-	 * @param content
-	 *            - the textual content of the Push/SMS message.
-	 * @return response - the HTTP response code of the performed action.
-	 */
-	public WhispirResponse sendMessage(String recipient, String subject, String content)
-			throws WhispirSDKException {
-		return sendMessage("", recipient, subject, content);
-	}
+  /**
+   * <p>
+   * Allows a user to send a message in the default My Company workspace.
+   * </p>
+   * <p>
+   * For more complex content, the user should use the Map content overloaded
+   * function.
+   * </p>
+   *
+   * @param recipient
+   *            - the mobile number or email address of the recipient of the
+   *            message
+   * @param subject
+   *            - the textual subject of the message
+   * @param content
+   *            - the textual content of the Push/SMS message.
+   * @return response - the HTTP response code of the performed action.
+   */
+  public WhispirResponse sendMessage(String recipient, String subject, String content)
+      throws WhispirSDKException {
+    return sendMessage("", recipient, subject, content);
+  }
 
-	/**
-	 * <p>
-	 * Allows a user to send a message in the specified Workspace ID.
-	 * </p>
-	 * <p>
-	 * For more complex content, the user should use the Map content overloaded
-	 * function.
-	 * </p>
-	 * 
-	 * @param recipient
-	 *            - the mobile number or email address of the recipient of the
-	 *            message
-	 * @param subject
-	 *            - the textual subject of the message
-	 * @param content
-	 *            - the textual content of the Push/SMS message.
-	 * @return response - the HTTP response code of the performed action.
-	 */
-	public WhispirResponse sendMessage(String workspaceId, String recipient,
-			String subject, String content) throws WhispirSDKException {
-		Map<String, String> smsContent = new HashMap<String, String>();
-		smsContent.put("body", content);
-		return sendMessage(workspaceId, recipient, subject, smsContent);
-	}
+  /**
+   * <p>
+   * Allows a user to send a message in the specified Workspace ID.
+   * </p>
+   * <p>
+   * For more complex content, the user should use the Map content overloaded
+   * function.
+   * </p>
+   *
+   * @param recipient
+   *            - the mobile number or email address of the recipient of the
+   *            message
+   * @param subject
+   *            - the textual subject of the message
+   * @param content
+   *            - the textual content of the Push/SMS message.
+   * @return response - the HTTP response code of the performed action.
+   */
+  public WhispirResponse sendMessage(String workspaceId, String recipient,
+      String subject, String content) throws WhispirSDKException {
+    Map<String, String> smsContent = new HashMap<String, String>();
+    smsContent.put("body", content);
+    return sendMessage(workspaceId, recipient, subject, smsContent);
+  }
 
-	/**
-	 * <p>
-	 * Allows a user to send a message in any workspace, with any combination of
-	 * content within the content map.
-	 * </p>
-	 * <p>
-	 * The content Map is expected to provide the following information.
-	 * </p>
-	 * <p>
-	 * For SMS/Push
-	 * </p>
-	 * <p>
-	 * - body - The content for the Push/SMS message.
-	 * </p>
-	 * 
-	 * @param recipient
-	 *            - the mobile number or email address of the recipient of the
-	 *            message
-	 * @param subject
-	 *            - the textual subject of the message
-	 * @param content
-	 *            - the Map of content for the Whispir Message
-	 * @return response - the HTTP response code of the performed action.
-	 */
-	public WhispirResponse sendMessage(String workspaceId, String recipient,
-			String subject, Map<String, String> content)
-			throws WhispirSDKException {
-		Map<String, String> options = new HashMap<String, String>();
-		return sendMessage(workspaceId, recipient, subject, content, options);
-	}
+  /**
+   * <p>
+   * Allows a user to send a message in any workspace, with any combination of
+   * content within the content map.
+   * </p>
+   * <p>
+   * The content Map is expected to provide the following information.
+   * </p>
+   * <p>
+   * For SMS/Push
+   * </p>
+   * <p>
+   * - body - The content for the Push/SMS message.
+   * </p>
+   *
+   * @param recipient
+   *            - the mobile number or email address of the recipient of the
+   *            message
+   * @param subject
+   *            - the textual subject of the message
+   * @param content
+   *            - the Map of content for the Whispir Message
+   * @return response - the HTTP response code of the performed action.
+   */
+  public WhispirResponse sendMessage(String workspaceId, String recipient,
+      String subject, Map<String, String> content)
+      throws WhispirSDKException {
+    Map<String, String> options = new HashMap<String, String>();
+    return sendMessage(workspaceId, recipient, subject, content, options);
+  }
 
-	/**
-	 * Allows a user to send a message in any workspace, with any combination of
-	 * content within the content map
-	 * <p>
-	 * The content Map is expected to provide the following information:
-	 * </p>
-	 * <p>
-	 * For SMS/Push
-	 * </p>
-	 * <p>
-	 * - body - The content of the SMS/Push Message
-	 * </p>
-	 * <p>
-	 * </p>
-	 * <p>
-	 * The options Map is expected to provide the following information:
-	 * </p>
-	 * <p>
-	 * - type - defaultNoReply (specifies if the message cannot be replied to)
-	 * </p>
-	 * <p>
-	 * - pushEscalation - true/false (string)
-	 * </p>
-	 * <p>
-	 * - escalationMins - 3,4,5,10 (string)
-	 * </p>
-	 * <p>
-	 * </p>
-	 * 
-	 * @param recipient
-	 *            - the mobile number or email address of the recipient of the
-	 *            message
-	 * @param subject
-	 *            - the textual subject of the message
-	 * @param content
-	 *            - the Map of content for the Whispir Message
-	 * @param options
-	 *            - the Map of options for the Whispir Message
-	 * @return response - the HTTP response code of the performed action.
-	 */
-	public WhispirResponse sendMessage(String workspaceId, String recipient,
-			String subject, Map<String, String> content,
-			Map<String, String> options) throws WhispirSDKException {
-		WhispirResponse response = new WhispirResponse();
+  /**
+   * Allows a user to send a message in any workspace, with any combination of
+   * content within the content map
+   * <p>
+   * The content Map is expected to provide the following information:
+   * </p>
+   * <p>
+   * For SMS/Push
+   * </p>
+   * <p>
+   * - body - The content of the SMS/Push Message
+   * </p>
+   * <p>
+   * </p>
+   * <p>
+   * The options Map is expected to provide the following information:
+   * </p>
+   * <p>
+   * - type - defaultNoReply (specifies if the message cannot be replied to)
+   * </p>
+   * <p>
+   * - pushEscalation - true/false (string)
+   * </p>
+   * <p>
+   * - escalationMins - 3,4,5,10 (string)
+   * </p>
+   * <p>
+   * </p>
+   *
+   * @param recipient
+   *            - the mobile number or email address of the recipient of the
+   *            message
+   * @param subject
+   *            - the textual subject of the message
+   * @param content
+   *            - the Map of content for the Whispir Message
+   * @param options
+   *            - the Map of options for the Whispir Message
+   * @return response - the HTTP response code of the performed action.
+   */
+  public WhispirResponse sendMessage(String workspaceId, String recipient,
+      String subject, Map<String, String> content,
+      Map<String, String> options) throws WhispirSDKException {
+    WhispirResponse response = new WhispirResponse();
 
-		if (recipient == null || recipient.length() < 8) {
-			// error with the recipient information, returning HTTP 422.
-			response.setStatusCode(422);
-			return response;
-		}
+    if (recipient == null || recipient.length() < 8) {
+      // error with the recipient information, returning HTTP 422.
+      response.setStatusCode(422);
+      return response;
+    }
 
-		try {
-			JSONObject request = new JSONObject();
+    try {
+      JSONObject request = new JSONObject();
 
-			request.put("to", recipient);
-			request.put("subject", subject);
+      request.put("to", recipient);
+      request.put("subject", subject);
 
-			// Check for the body in the map
-			if (content.containsKey("body") && !"".equals(content.get("body"))) {
-				request.put("body", content.get("body"));
-			}
+      // Check for the body in the map
+      if (content.containsKey("body") && !"".equals(content.get("body"))) {
+        request.put("body", content.get("body"));
+      }
 
-			// Check for the email in the map
-			if (content.containsKey("email")
-					&& !"".equals(content.get("email"))) {
-				JSONObject email = new JSONObject();
-				email.put("body", content.get("email"));
+      // Check for the email in the map
+      if (content.containsKey("email")
+          && !"".equals(content.get("email"))) {
+        JSONObject email = new JSONObject();
+        email.put("body", content.get("email"));
 
-				if (content.containsKey("emailType")
-						&& !"".equals(content.get("emailType"))) {
-					email.put("type", content.get("emailType"));
-				}
+        if (content.containsKey("emailType")
+            && !"".equals(content.get("emailType"))) {
+          email.put("type", content.get("emailType"));
+        }
 
-				request.put("email", email);
-			}
+        request.put("email", email);
+      }
 
-			// Check for the voice content in the map
-			if (content.containsKey("voice")
-					&& !"".equals(content.get("voice"))) {
-				JSONObject voice = new JSONObject();
-				voice.put("body", content.get("voice"));
-				voice.put("type",
-						"ConfCall:,ConfAccountNo:,ConfPinNo:,ConfModPinNo:,Pin:");
+      // Check for the voice content in the map
+      if (content.containsKey("voice")
+          && !"".equals(content.get("voice"))) {
+        JSONObject voice = new JSONObject();
+        voice.put("body", content.get("voice"));
+        voice.put("type",
+            "ConfCall:,ConfAccountNo:,ConfPinNo:,ConfModPinNo:,Pin:");
 
-				if (content.containsKey("voiceIntro")
-						&& !"".equals(content.get("voiceIntro"))) {
-					voice.put("header", content.get("voiceIntro"));
-				}
+        if (content.containsKey("voiceIntro")
+            && !"".equals(content.get("voiceIntro"))) {
+          voice.put("header", content.get("voiceIntro"));
+        }
 
-				request.put("voice", voice);
-			}
+        request.put("voice", voice);
+      }
 
-			// Check for the web content in the map
-			if (content.containsKey("web") && !"".equals(content.get("web"))) {
-				JSONObject web = new JSONObject();
-				web.put("body", content.get("web"));
+      // Check for the web content in the map
+      if (content.containsKey("web") && !"".equals(content.get("web"))) {
+        JSONObject web = new JSONObject();
+        web.put("body", content.get("web"));
 
-				if (content.containsKey("webType")
-						&& !"".equals(content.get("webType"))) {
-					web.put("type", content.get("webType"));
-				}
+        if (content.containsKey("webType")
+            && !"".equals(content.get("webType"))) {
+          web.put("type", content.get("webType"));
+        }
 
-				request.put("web", web);
-			}
+        request.put("web", web);
+      }
 
-			// Check for the noreply options in the map
-			if (options.containsKey("type")) {
-				request.put("type", options.get("type"));
-			}
+      // Check for the noreply options in the map
+      if (options.containsKey("type")) {
+        request.put("type", options.get("type"));
+      }
 
-			// Check for the push to SMS escalation options in the map
-			if (options.containsKey("pushNotifications")
-					&& "enabled".equalsIgnoreCase(options
-							.get("pushNotifications"))) {
+      // Check for the push to SMS escalation options in the map
+      if (options.containsKey("pushNotifications")
+          && "enabled".equalsIgnoreCase(options
+              .get("pushNotifications"))) {
 
-				JSONObject pushOptions = new JSONObject();
-				pushOptions.put("notifications", "enabled");
+        JSONObject pushOptions = new JSONObject();
+        pushOptions.put("notifications", "enabled");
 
-				if (options.containsKey("pushEscalationMins")) {
-					pushOptions.put("escalationMins",
-							options.get("pushEscalationMins"));
-				}
+        if (options.containsKey("pushEscalationMins")) {
+          pushOptions.put("escalationMins",
+              options.get("pushEscalationMins"));
+        }
 
-				JSONObject features = new JSONObject();
-				features.put("pushOptions", pushOptions);
+        JSONObject features = new JSONObject();
+        features.put("pushOptions", pushOptions);
 
-				request.put("features", features);
-			}
+        request.put("features", features);
+      }
 
-			// System.out.println("Request: " + request.toString());
+      // System.out.println("Request: " + request.toString());
 
-			// Execute the request
-			return sdk.post(WhispirSDKConstants.MESSAGES_RESOURCE, workspaceId, request.toString());
-	
-		} catch (JSONException e) {
-			throw new WhispirSDKException(
-					"Error occurred parsing the object with the content provided."
-							+ e.getMessage());
-		}
-	}
-	
-	/**
-	 * <p>
-	 * Allows a user to retrieve a list of messages.
-	 * </p>
-	 * 
-	 * @return response - the WhispirResponse object of the performed action.
-	 */
-	public WhispirResponse getMessages() throws WhispirSDKException {
-		WhispirResponse response = new WhispirResponse();
-		
-		response = sdk.get(WhispirSDKConstants.MESSAGES_RESOURCE, "");
-		
-		Map<String, String> map = new TreeMap<String, String>();
+      // Execute the request
+      return sdk.post(WhispirSDKConstants.MESSAGES_RESOURCE, workspaceId, request.toString());
 
-		try {
-			JSONObject obj = new JSONObject(response.getRawResponse());
+    } catch (JSONException e) {
+      throw new WhispirSDKException(
+          "Error occurred parsing the object with the content provided."
+              + e.getMessage());
+    }
+  }
 
-			JSONArray messages = obj.getJSONArray("messages");
-			int messagesLength = messages.length();
+  /**
+   * <p>
+   * Allows a user to retrieve a list of messages.
+   * </p>
+   *
+   * @return response - the WhispirResponse object of the performed action.
+   */
+  public WhispirResponse getMessages() throws WhispirSDKException {
+    WhispirResponse response = new WhispirResponse();
 
-			for (int i = 0; i < messagesLength; i++) {
-				String subject = (String) messages.getJSONObject(i).get(
-						"subject");
-				String fullUrl = (String) messages.getJSONObject(i)
-						.getJSONArray("link").getJSONObject(0).get("uri");
+    response = sdk.get(WhispirSDKConstants.MESSAGES_RESOURCE, "");
 
-				String id = fullUrl.substring(fullUrl.lastIndexOf("/") + 1,
-						fullUrl.lastIndexOf("?"));
+    Map<String, String> map = new TreeMap<String, String>();
 
-				System.out.println(subject + " " + id);
+    try {
+      JSONObject obj = new JSONObject(response.getRawResponse());
 
-				map.put(subject, id);
-			}
+      JSONArray messages = obj.getJSONArray("messages");
+      int messagesLength = messages.length();
 
-		} catch (JSONException e) {
-			throw new WhispirSDKException(e.getMessage());
-		}
+      for (int i = 0; i < messagesLength; i++) {
+        String subject = (String) messages.getJSONObject(i).get(
+            "subject");
+        String fullUrl = (String) messages.getJSONObject(i)
+            .getJSONArray("link").getJSONObject(0).get("uri");
 
-		response.setResponse(map);
+        String id = fullUrl.substring(fullUrl.lastIndexOf("/") + 1,
+            fullUrl.lastIndexOf("?"));
 
-		return response;
-	}
-	
-	/**
-	 * <p>
-	 * Allows a user to retrieve a single message from the API.
-	 * </p>
-	 * @param messageId - the ID of the message to retrieve
-	 * @return response - the WhispirResponse object of the performed action.
-	 */
-	public WhispirResponse getMessage(String messageId) throws WhispirSDKException {
-		WhispirResponse response = new WhispirResponse();
-		
-		response = sdk.get(WhispirSDKConstants.MESSAGES_RESOURCE, "", messageId);
-		
-		Map<String, String> map = new TreeMap<String, String>();
+        System.out.println(subject + " " + id);
 
-		response.setResponse(map);
+        map.put(subject, id);
+      }
 
-		return response;
-	}
+    } catch (JSONException e) {
+      throw new WhispirSDKException(e.getMessage());
+    }
+
+    response.setResponse(map);
+
+    return response;
+  }
+
+  /**
+   * <p>
+   * Allows a user to retrieve a single message from the API.
+   * </p>
+   * @param messageId - the ID of the message to retrieve
+   * @return response - the WhispirResponse object of the performed action.
+   */
+  public WhispirResponse getMessage(String messageId) throws WhispirSDKException {
+    WhispirResponse response = new WhispirResponse();
+
+    response = sdk.get(WhispirSDKConstants.MESSAGES_RESOURCE, "", messageId);
+
+    Map<String, String> map = new TreeMap<String, String>();
+
+    response.setResponse(map);
+
+    return response;
+  }
 }
--- src/test/java/com/whispir/sdk/tests/WhispirSDKTest.java
+++ src/test/java/com/whispir/sdk/tests/WhispirSDKTest.java
@@ -11,136 +11,136 @@ import com.whispir.sdk.WhispirSDK;
 import com.whispir.sdk.exceptions.WhispirSDKException;
 
 public class WhispirSDKTest {
-	
-	protected WhispirSDK whispirSDK;
- 
-	// POPULATE THESE OR ALL THE TESTS WILL FAIL!!!
 
-	protected static final String TEST_API_KEY = "";
-	protected static final String TEST_USERNAME = "";
-	protected static final String TEST_PASSWORD = "";
+  protected WhispirSDK whispirSDK;
 
-	// Message content variables for the tests
-	protected static final String TEST_RECIPIENT = "";
-	protected static final String TEST_WORKSPACE_ID = "";
-	protected static final String TEST_WORKSPACE_NAME = "";
-	protected static final String TEST_MESSAGE_SUBJECT = "";
-	protected static final String TEST_MESSAGE_BODY = "";
+  // POPULATE THESE OR ALL THE TESTS WILL FAIL!!!
 
-	// Debugging
-	protected static final String DEBUG_HOST = "";
-	
-	// Proxy Debugging (I installed Squid on my Mac from here http://squidman.net/squidman/index.html)
-	protected static final String PROXY_HOST = "";
-	protected static final int PROXY_PORT = 0;
-	protected static final boolean PROXY_HTTPS_ENABLED = false;
-	protected static final String PROXY_USERNAME = "";
-	protected static final String PROXY_PASSWORD = "";
-	
-	//Squid Proxy Error Code is 407 (Proxy Authentication Required)
-	protected static final int PROXY_ERROR_CODE = 407;
+  protected static final String TEST_API_KEY = "";
+  protected static final String TEST_USERNAME = "";
+  protected static final String TEST_PASSWORD = "";
 
-	@Before
-	public void setUp() throws Exception {
-		if (!"".equals(DEBUG_HOST)) {
-			this.whispirSDK = new WhispirSDK(TEST_API_KEY, TEST_USERNAME,
-					TEST_PASSWORD, DEBUG_HOST);
-		} else {
-			this.whispirSDK = new WhispirSDK(TEST_API_KEY, TEST_USERNAME,
-					TEST_PASSWORD);
-		}
-		
-		if (!"".equals(PROXY_HOST)) {
-			
-			if(!"".equals(PROXY_USERNAME) && !"".equals(PROXY_PASSWORD)) {
-				this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED, PROXY_USERNAME, PROXY_PASSWORD);
-			} else {
-				this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED);
-			}
-		}
-	}
+  // Message content variables for the tests
+  protected static final String TEST_RECIPIENT = "";
+  protected static final String TEST_WORKSPACE_ID = "";
+  protected static final String TEST_WORKSPACE_NAME = "";
+  protected static final String TEST_MESSAGE_SUBJECT = "";
+  protected static final String TEST_MESSAGE_BODY = "";
 
-	@After
-	public void tearDown() throws Exception {
-		this.whispirSDK = null;
-	}
-	
-	@Test
-	public void testBadProxyUsername() throws WhispirSDKException {
-		
-		if(!"".equals(PROXY_HOST)) {
-			this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED, "bad", PROXY_PASSWORD);
-			
-			this.whispirSDK.setApikey(TEST_API_KEY);
-			this.whispirSDK.setUsername(TEST_USERNAME);
-			this.whispirSDK.setPassword(TEST_PASSWORD);
-			
-			WhispirResponse response = this.whispirSDK.getWorkspaces();
+  // Debugging
+  protected static final String DEBUG_HOST = "";
 
-			assertTrue(response.getStatusCode() == PROXY_ERROR_CODE);
-		} else {
-			assertTrue(true);
-		}
-	}
-	
-	@Test
-	public void testBadProxyPassword() throws WhispirSDKException {
-		
-		if(!"".equals(PROXY_HOST)) {
-			this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED, PROXY_USERNAME, "bad");
-			
-			this.whispirSDK.setApikey(TEST_API_KEY);
-			this.whispirSDK.setUsername(TEST_USERNAME);
-			this.whispirSDK.setPassword(TEST_PASSWORD);
-			
-			WhispirResponse response = this.whispirSDK.getWorkspaces();
+  // Proxy Debugging (I installed Squid on my Mac from here http://squidman.net/squidman/index.html)
+  protected static final String PROXY_HOST = "";
+  protected static final int PROXY_PORT = 0;
+  protected static final boolean PROXY_HTTPS_ENABLED = false;
+  protected static final String PROXY_USERNAME = "";
+  protected static final String PROXY_PASSWORD = "";
 
-			
-			assertTrue(response.getStatusCode() == PROXY_ERROR_CODE);
-		}
-		
-		assertTrue(true);
-	}
+  //Squid Proxy Error Code is 407 (Proxy Authentication Required)
+  protected static final int PROXY_ERROR_CODE = 407;
 
-	@Test
-	public void testBadAPIKey() throws WhispirSDKException {
-		this.whispirSDK.setApikey("1234");
-		this.whispirSDK.setUsername(TEST_USERNAME);
-		this.whispirSDK.setPassword(TEST_PASSWORD);
-		
-		WhispirResponse response = this.whispirSDK.sendMessage(TEST_RECIPIENT, TEST_MESSAGE_SUBJECT, TEST_MESSAGE_BODY);
+  @Before
+  public void setUp() throws Exception {
+    if (!"".equals(DEBUG_HOST)) {
+      this.whispirSDK = new WhispirSDK(TEST_API_KEY, TEST_USERNAME,
+          TEST_PASSWORD, DEBUG_HOST);
+    } else {
+      this.whispirSDK = new WhispirSDK(TEST_API_KEY, TEST_USERNAME,
+          TEST_PASSWORD);
+    }
 
-		if(!"".equals(DEBUG_HOST)) {
-			assertTrue(response.getStatusCode() == 202);
-		} else {
-			// HTTP403 Permission Denied / Forbidden
-			assertTrue(response.getStatusCode() == 403);
-		}
-	}
+    if (!"".equals(PROXY_HOST)) {
 
-	@Test
-	public void testBadUsername() throws WhispirSDKException {
-		this.whispirSDK.setApikey(TEST_API_KEY);
-		this.whispirSDK.setUsername("blahblahblah");
-		this.whispirSDK.setPassword(TEST_PASSWORD);
-		
-		WhispirResponse response = this.whispirSDK.sendMessage(TEST_RECIPIENT,
-				TEST_MESSAGE_SUBJECT, TEST_MESSAGE_BODY);
+      if(!"".equals(PROXY_USERNAME) && !"".equals(PROXY_PASSWORD)) {
+        this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED, PROXY_USERNAME, PROXY_PASSWORD);
+      } else {
+        this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED);
+      }
+    }
+  }
 
-		// HTTP401 Unauthorized Access
-		assertTrue(response.getStatusCode() == 401);
-	}
+  @After
+  public void tearDown() throws Exception {
+    this.whispirSDK = null;
+  }
 
-	@Test
-	public void testBadPassword() throws WhispirSDKException {
-		this.whispirSDK.setApikey(TEST_API_KEY);
-		this.whispirSDK.setUsername(TEST_USERNAME);
-		this.whispirSDK.setPassword("blahblahblah");
-		
-		WhispirResponse response = this.whispirSDK.sendMessage(TEST_RECIPIENT,
-				TEST_MESSAGE_SUBJECT, TEST_MESSAGE_BODY);
+  @Test
+  public void testBadProxyUsername() throws WhispirSDKException {
 
-		// HTTP401 Unauthorized Access
-		assertTrue(response.getStatusCode() == 401);
-	}
+    if(!"".equals(PROXY_HOST)) {
+      this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED, "bad", PROXY_PASSWORD);
+
+      this.whispirSDK.setApikey(TEST_API_KEY);
+      this.whispirSDK.setUsername(TEST_USERNAME);
+      this.whispirSDK.setPassword(TEST_PASSWORD);
+
+      WhispirResponse response = this.whispirSDK.getWorkspaces();
+
+      assertTrue(response.getStatusCode() == PROXY_ERROR_CODE);
+    } else {
+      assertTrue(true);
+    }
+  }
+
+  @Test
+  public void testBadProxyPassword() throws WhispirSDKException {
+
+    if(!"".equals(PROXY_HOST)) {
+      this.whispirSDK.setProxy(PROXY_HOST, PROXY_PORT, PROXY_HTTPS_ENABLED, PROXY_USERNAME, "bad");
+
+      this.whispirSDK.setApikey(TEST_API_KEY);
+      this.whispirSDK.setUsername(TEST_USERNAME);
+      this.whispirSDK.setPassword(TEST_PASSWORD);
+
+      WhispirResponse response = this.whispirSDK.getWorkspaces();
+
+
+      assertTrue(response.getStatusCode() == PROXY_ERROR_CODE);
+    }
+
+    assertTrue(true);
+  }
+
+  @Test
+  public void testBadAPIKey() throws WhispirSDKException {
+    this.whispirSDK.setApikey("1234");
+    this.whispirSDK.setUsername(TEST_USERNAME);
+    this.whispirSDK.setPassword(TEST_PASSWORD);
+
+    WhispirResponse response = this.whispirSDK.sendMessage(TEST_RECIPIENT, TEST_MESSAGE_SUBJECT, TEST_MESSAGE_BODY);
+
+    if(!"".equals(DEBUG_HOST)) {
+      assertTrue(response.getStatusCode() == 202);
+    } else {
+      // HTTP403 Permission Denied / Forbidden
+      assertTrue(response.getStatusCode() == 403);
+    }
+  }
+
+  @Test
+  public void testBadUsername() throws WhispirSDKException {
+    this.whispirSDK.setApikey(TEST_API_KEY);
+    this.whispirSDK.setUsername("blahblahblah");
+    this.whispirSDK.setPassword(TEST_PASSWORD);
+
+    WhispirResponse response = this.whispirSDK.sendMessage(TEST_RECIPIENT,
+        TEST_MESSAGE_SUBJECT, TEST_MESSAGE_BODY);
+
+    // HTTP401 Unauthorized Access
+    assertTrue(response.getStatusCode() == 401);
+  }
+
+  @Test
+  public void testBadPassword() throws WhispirSDKException {
+    this.whispirSDK.setApikey(TEST_API_KEY);
+    this.whispirSDK.setUsername(TEST_USERNAME);
+    this.whispirSDK.setPassword("blahblahblah");
+
+    WhispirResponse response = this.whispirSDK.sendMessage(TEST_RECIPIENT,
+        TEST_MESSAGE_SUBJECT, TEST_MESSAGE_BODY);
+
+    // HTTP401 Unauthorized Access
+    assertTrue(response.getStatusCode() == 401);
+  }
 }
